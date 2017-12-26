c1819cd76e50320e514423a66f5f60efaf2ff207
Dhruv Mehta <dhruvnm@umd.edu>
2017-12-26 17:26:29 -0500

    simple page that accepts comma seperated lists of classes and then prints the associated json from umd.io to the terminal

--- src/jupiter.py
+++ src/jupiter.py
@@ -1,7 +1,32 @@
 import json
 from pprint import pprint
-import urllib.request
+from urllib.request import urlopen
+from flask import Flask, render_template, request
 
-data = urllib.request.urlopen('http://api.umd.io/v0/courses/math410/sections')
-data = json.load(data)
-pprint(data)
+app = Flask(__name__)
+url = 'http://api.umd.io/v0/courses/'
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return render_template('submit.html')
+    else:
+        result = request.form
+        get_classes(result['classes'])
+        return result['classes']
+
+def get_classes(classes):
+    classes = classes.split(',')
+    mod = list()
+    for c in classes:
+        c = c.strip()
+        c = c.lower()
+        mod.append(c)
+
+    for c in mod:
+        c_url = ''.join([url, c, '?expand=sections'])
+        result = urlopen(c_url)
+        pprint(json.load(result))
+
+if __name__ == '__main__':
+    app.run()
