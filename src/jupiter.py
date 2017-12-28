e919d35cad6f340c79dca44ca511491214a56091
Dhruv Mehta <dhruvnm@umd.edu>
2017-12-27 22:16:47 -0500

    added code to generate all possible schedules. untested.

--- src/jupiter.py
+++ src/jupiter.py
@@ -1,7 +1,10 @@
-import json
+from json import load
 from pprint import pprint
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from flask import Flask, render_template, request
+from sections import Section, Meeting
+from itertools import product
 
 app = Flask(__name__)
 url = 'http://api.umd.io/v0/courses/'
@@ -23,10 +26,66 @@ def get_classes(classes):
         c = c.lower()
         mod.append(c)
 
+    class_json = list()
+
     for c in mod:
-        c_url = ''.join([url, c, '?expand=sections'])
-        result = urlopen(c_url)
-        pprint(json.load(result))
+        c_url = ''.join([url, c, '/sections'])
+        try:
+            result = urlopen(c_url)
+        except HTTPError:
+            continue
+
+        class_info = load(result)
+        class_json.append(class_info)
+
+    class_json.sort(key=len)
+    make_schedules(class_json)
+
+def make_schedules(classes):
+    # No schedules possible if there are no classes
+    if len(classes) is 0:
+        return None
+
+    schedules = convert(classes[0])
+
+    i = 1;
+    while i < len(classes):
+        new_list = convert(classes[i]) # next classes sections
+        # cartesian product of current schedules with new sections to get all
+        # new possible sections
+        schedules = product(schedules, new_list)
+        conflicts = list()
+        j = 0
+        # iterate over all schedules to detect conflicts
+        while j < len(schedules):
+            for section in schedules[j][0]: # iterates over all old classes in schedule
+                # checks to see if new section is a conflict
+                if schedules[j][1].is_conflict(section) is True:
+                    conflicts.append(j)
+                    break
+            j = j + 1
+
+        # remove all schedules with conflicts
+        conflicts = list(reversed(conflicts))
+        for idx in conflicts:
+            schedules.pop(idx)
+
+        # flatten list. i.e [((1,), (2,))] -> [(1, 2,)]
+        new_schedules = list()
+        for elt in schedules:
+            new_schedules.append(tuple([section for tupl in elt for section in tupl]))
+        schedules = new_schedules
+        i = i + 1
+
+# Converts a list of sections into a list of section objects in a tuple
+def convert(sections):
+    obj_list = list()
+    for section in sections:
+        section_obj = Section(section['section_id'], section['instructors'])
+        for meeting in section['meetings']:
+            section_obj.add_meeting(Meeting(meeting['days'], meeting['start_time'], meeting['end_time']))
+        obj_list.append((section_obj,))
+    return obj_list
 
 if __name__ == '__main__':
     app.run()
---
+++ src/sections.py
@@ -0,0 +1,58 @@
+from datetime import time
+from re import findall
+
+class Section:
+    def __init___(self, section_id, instructors):
+        self.meetings = list()
+        self.section_id = section_id
+        self.instructors = instructors
+
+    def add_meeting(self, meeting):
+        self.meetings.append(meeting)
+
+    # Check if any meetings in this section object conflict with any meetings in
+    # another section object
+    def is_conflict(self, other):
+        for other_meeting in other.meetings:
+            for this_meeting in self.meetings:
+                if (other_meeting.is_conflict(this_meeting)) is True:
+                    return True
+        return False
+
+class Meeting:
+    def __init__(self, days, start_time, end_time):
+        self.days = days
+
+        # The following code parses a string like 12:30pm and saves it as a time object
+        t = start_time.split(':')
+        if start_time[-2:] is 'am':
+            self.start_time = time(int(t[0]), int(t[1][:-2])))
+        else:
+            self.start_time = time(int(t[0]) + 12, int(t[1][:-2]))
+
+        t = end_time.split(':')
+        if end_time[-2:] is 'am':
+            self.end_time = time(int(t[0]), int(t[1][:-2])))
+        else:
+            self.end_time = time(int(t[0]) + 12, int(t[1][:-2]))
+
+    def is_conflict(self, other):
+        # Split 'MWF' into ['M', 'W', 'F'], etc.
+        other_days = findall('[A-Z][^A-Z]*', other.days)
+
+        # Check if the two meetings share any days
+        day_conflict = False
+        for day in other_days:
+            if day in self.days:
+                day_conflict = True
+                break
+
+        # if there is a day conlict, check the times
+        if day_conflict is True:
+            if (other.start_time >= self.start_time and
+               other.start_time <= self.end_time) or
+               (other.end_time >= self.start_time and
+               other.end_time <= self.end_time):
+                return True
+
+        return False
